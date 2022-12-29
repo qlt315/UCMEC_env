4d2ad9d61fbc87454a9e6e3307a96a6a6c7f4561
Langtian Qin <52751833+qlt315@users.noreply.github.com>
2022-12-29 21:44:19 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-import setuptools  # 导入setuptools打包工具
+import setuptools 
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="UCMEC",
-    version="1.0.0",
+    version="1.0.2",
     author="Langtian Qin",
     author_email="qlt315@mail.ustc.edu.com",
     description="Reinforcement Learning Environment for User-centric Mobile Edge Computing ",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/qt315/Multi-agent-UCMEC",  # 自己项目地址，比如github的项目地址
+    url="https://github.com/qlt315/UCMEC_env", 
     packages=setuptools.find_packages(),
     classifiers=[
         "Intended Audience :: Developers",
